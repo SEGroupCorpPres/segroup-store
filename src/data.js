8e74c3e736cfab28f74664086bb77cfb71f9faf4
SEGroupCorpPres <artessdu@gmail.com>
2021-07-23 23:28:43 +0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,124 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Samsung Galaxy Note 20 Ultra",
+        images: "images/Samsung_Galaxy_Note_20_Ultra_512_Black.jpg",
+        price: 1484.56,
+        company: "Samsung",
+        info: "Стоимость предзаказа 1 000 000 сум" + 
+
+            "Смартфон Samsung Galaxy Note 20 Ultra 256 ГБ Black" +
+        
+            "Профессиональная камера смартфона Samsung Galaxy Note 20 Ultra Black с тремя объективами имеет множество современный функций. С помощью таких камер вы сможете создавать видео профессионального уровня потрясающей чёткости и детализации. Вы также сможете воспользоваться самыми разнообразными режимами и функциями, для создания своих шедевров. К тому же, с помощью режима стабилизации вы сможете добавлять снимкам больше чёткости и насыщенности. Кроме того, 5-кратный гибридный оптический зум позволит увеличивать кадры в 50 раз, при этом, не теряя чёткости снимка. А ультра-широкоугольная камера позволит запечатлеть пейзаж или большое здание целиком, значительно расширяя угол обзора."+
+        
+        
+            "Впечатляющий девайс откроет вам абсолютно новый мир со множеством возможностей. Это первый смартфон, объединивший в себе электронное перо. В конструкции девайса предусмотрен обновлённый S Pen, который отличается высокой скоростью отклика, создавая ощущение лёгкого пера. К тому же, такое «перо» имеет возможности подключения к Bluetooth и синхронизации на вашем ПК посредством фирменного приложения OneNote. Металлический корпус устройства идеально дополняет оригинальный камерный блок. Яркий 6.9-дюймовый дисплей отображает насыщенные и детализированные изображения с естественными оттенками даже при сильном солнечном свете."+
+        
+        
+            "Данная модель работает на базе мощного интеллектуального процессора Samsung Exynos, который обеспечивает высокую производительность всего устройства. Кроме того, для установки и хранения огромного количества приложений, игр, видео и других файлов – в нём предусмотрена встроенная память на 256 ГБ. А его оперативная память на 8 ГБ отлично справляется со стабильной работой в многозадачном режиме. Более того, встроенная система защиты Samsung Knox обеспечивает комплексную защиту аппаратного и программного обеспечения для безопасности ваших данных в режиме реального времени."+
+        
+        
+            "Аккумулятор устройства с невероятной ёмкостью 4500 мАч оснащён системой интеллектуального энергопотребления, которая эффективно продлевает работу смартфона. А быстрая зарядка мощностью 25 Вт всего за 30 минут способна зарядить девайс на 50%, а также вы сможете воспользоваться функцией беспроводной зарядки PowerShare."+
+        
+            "Купить Смартфон Samsung Galaxy Note 20 Ultra 256 ГБ Black вы можете в нашем интернет-магазине MEDIAPARK.UZ."+
+        
+            "* Характеристики и комплектация могут быть изменены фирмой-производителем без предварительного уведомления.",
+        inCart: false,
+        count: 0,
+        total: 0 
+    },
+    {
+        id: 2,
+        title: "Apple iPhone 12 Pro Max",
+        images: "images/Apple iPhone 12 Pro Max.png",
+        price: 1484.56,
+        company: "Apple",
+        info: "The fastest chip ever in a smartphone. Again." +
+        "The new capabilities of iPhone 12 Pro require speed. Lots of it. And one way to make a chip faster is to add more transistors, meaning you have to make the chip bigger or the transistors smaller." +
+        
+        "11.8 billion" +
+        "transistors" +
+       "We went for smaller. iPhone 12 Pro has the first 5‑nanometer chip in the industry. The components are measured in mere atoms, which means the transistors can be packed incredibly close together. So energy flows between them faster — and less of it gets lost along the way. That’s how A14 Bionic boosts performance while saving energy." +
+        
+        "16‑core" +
+        "Neural Engine 11 trillion operations per second" +
+        "We dramatically increased the performance of our Neural Engine by doubling the cores from 8 to 16. That makes intensive machine learning tasks — like applying Deep Fusion to improve detail in your photos — up to 80% faster. We also made the dedicated machine learning accelerators on our CPU 70% faster, so any app can tap into that improved performance across our entire chip." +
+        
+        "All-new" +
+        "image signal processor 4K HDR recording with Dolby Vision" +
+        "What does all this mean for you? A ton. Big performance gains across the board. Smart HDR 3 for more true-to-life photos. Improved detail in high-quality video, thanks to advanced temporal noise reduction on the new Image Signal Processor. And Dolby Vision that’s actually encoded while you shoot — instead of by pro computers in an editing studio. There’s never been anything like A14 Bionic, and we can’t wait to see all the incredible things you’ll do with it.",
+        inCart: false,
+        count: 0,
+        total: 0 
+    },
+    {
+        id: 3,
+        title: "Apple iPhone 12 Pro Max Gold",
+        images: "images/Apple iPhone 12 Pro Max Gold.png",
+        price: 1484.56,
+        company: "Apple",
+        info: "The fastest chip ever in a smartphone. Again." +
+        "The new capabilities of iPhone 12 Pro require speed. Lots of it. And one way to make a chip faster is to add more transistors, meaning you have to make the chip bigger or the transistors smaller." +
+        
+        "11.8 billion" +
+        "transistors" +
+       "We went for smaller. iPhone 12 Pro has the first 5‑nanometer chip in the industry. The components are measured in mere atoms, which means the transistors can be packed incredibly close together. So energy flows between them faster — and less of it gets lost along the way. That’s how A14 Bionic boosts performance while saving energy." +
+        
+        "16‑core" +
+        "Neural Engine 11 trillion operations per second" +
+        "We dramatically increased the performance of our Neural Engine by doubling the cores from 8 to 16. That makes intensive machine learning tasks — like applying Deep Fusion to improve detail in your photos — up to 80% faster. We also made the dedicated machine learning accelerators on our CPU 70% faster, so any app can tap into that improved performance across our entire chip." +
+        
+        "All-new" +
+        "image signal processor 4K HDR recording with Dolby Vision" +
+        "What does all this mean for you? A ton. Big performance gains across the board. Smart HDR 3 for more true-to-life photos. Improved detail in high-quality video, thanks to advanced temporal noise reduction on the new Image Signal Processor. And Dolby Vision that’s actually encoded while you shoot — instead of by pro computers in an editing studio. There’s never been anything like A14 Bionic, and we can’t wait to see all the incredible things you’ll do with it.",
+        inCart: false,
+        count: 0,
+        total: 0 
+    },
+    {
+        id: 4,
+        title: "Apple iPhone 11 Pro Max",
+        images: "images/Apple iPhone 11 Pro Max.jpg",
+        price: 1484.56,
+        company: "Apple",
+        info: "sdfasdasdsadas",
+        inCart: false,
+        count: 0,
+        total: 0 
+    },
+    {
+        id: 5,
+        title: "Samsung Galaxy Note 10+",
+        images: "images/Samsung Galaxy Note 10+.jpg",
+        price: 1484.56,
+        company: "Samsung",
+        info: "dsadsadsadasdsadadads",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+]
+export const detailProduct = {
+    id: 3,
+    title: "Apple iPhone 12 Pro Max Gold",
+    images: "images/Apple iPhone 12 Pro Max Gold.png",
+    price: 1484.56,
+    company: "Apple",
+    info: "The fastest chip ever in a smartphone. Again." +
+        "The new capabilities of iPhone 12 Pro require speed. Lots of it. And one way to make a chip faster is to add more transistors, meaning you have to make the chip bigger or the transistors smaller." +
+        
+        "11.8 billion" +
+        "transistors" +
+       "We went for smaller. iPhone 12 Pro has the first 5‑nanometer chip in the industry. The components are measured in mere atoms, which means the transistors can be packed incredibly close together. So energy flows between them faster — and less of it gets lost along the way. That’s how A14 Bionic boosts performance while saving energy." +
+        
+        "16‑core" +
+        "Neural Engine 11 trillion operations per second" +
+        "We dramatically increased the performance of our Neural Engine by doubling the cores from 8 to 16. That makes intensive machine learning tasks — like applying Deep Fusion to improve detail in your photos — up to 80% faster. We also made the dedicated machine learning accelerators on our CPU 70% faster, so any app can tap into that improved performance across our entire chip." +
+        
+        "All-new" +
+        "image signal processor 4K HDR recording with Dolby Vision" +
+        "What does all this mean for you? A ton. Big performance gains across the board. Smart HDR 3 for more true-to-life photos. Improved detail in high-quality video, thanks to advanced temporal noise reduction on the new Image Signal Processor. And Dolby Vision that’s actually encoded while you shoot — instead of by pro computers in an editing studio. There’s never been anything like A14 Bionic, and we can’t wait to see all the incredible things you’ll do with it.",
+    inCart: false,
+    count: 0,
+    total: 0
+}
